b40bab200a742398aac4a9604d223cf326549f42
CarlosAcor <carlosAcor@ext.inditex.com>
2024-07-15 13:04:07 +0200

    feat: Visualize video on detail view from BE

---
+++ src/pages/api/retrievevideo.ts
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Profile } from '@/types';
+
+export type RetrieveResponse = {
+  url: string;
+  tag: Profile;
+  destination: string;
+};
+
+export function retrieveVideoApiRequest(): Promise<Response> {
+  return fetch('/api/retrievevideo', { method: 'GET' });
+}
+
+
+export default async function handler(
+  _req: NextApiRequest,
+  _res: NextApiResponse<RetrieveResponse[]>,
+) {
+  const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+  const res = await fetch(`${serverUrl}/videos`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  const json = await res.json();
+  console.log('Retrieve: ', json);
+  _res.status(200).json(json);
+}
