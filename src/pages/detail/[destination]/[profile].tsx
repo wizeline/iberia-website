7172cc43424883740441226757b801e8236a3934
Daniel Hernández Alcojor <daniel.hernandez@wizeline.com>
2024-07-16 08:38:52 +0200

    chore: Specify type, code cleanup

--- src/pages/detail/[destination]/[profile].tsx
+++ src/pages/detail/[destination]/[profile].tsx
@@ -1,129 +1,129 @@
 import Header from '@/components/header/header';
-import { DESTINATIONS, PROFILES } from '@/constants';
-import { FormEvent, useEffect, useState } from 'react';
-import { useLocalStorage } from 'usehooks-ts';
-import { LocalStorageImgUrls, Profile } from '@/types';
-import { addUrl } from '@/utils';
-import styles from './index.module.css';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { retrieveVideoApiRequest, RetrieveResponse } from '@/pages/api/retrievevideo';
-
+import {
+  RetrieveResponse,
+  retrieveVideoApiRequest,
+} from '@/pages/api/retrievevideo';
 
 const AdminPage = () => {
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState<RetrieveResponse[]>([]);
 
   const router = useRouter();
   const { destination, profile } = router.query;
   const handleBackClick = () => {
-          router.push(`/`);
+    router.push(`/`);
   };
-  
+
   function retrieveVideo() {
-      retrieveVideoApiRequest()
-        .then((response) => {
-          if (response.status === 200) {
-            response.json().then((data) => {
-              setVideos(
-                data
-              );
-            });
-          } 
+    retrieveVideoApiRequest().then((response) => {
+      if (response.status === 200) {
+        response.json().then((data) => {
+          setVideos(data);
         });
-        
+      }
+    });
   }
-  
+
   useEffect(() => {
     retrieveVideo();
-  },[]);
-  
-  console.log("profile:",profile);
-  console.log("destination:",destination);
-  console.log("url:",videos.filter(item => item.tag === profile && item.destination === destination)[0]);
-     
-  const backgroundVideo = videos.filter(item => item.tag === profile && item.destination === destination)[0]?.url
-                                                                 
+  }, []);
+
+  console.log('profile:', profile);
+  console.log('destination:', destination);
+  console.log(
+    'url:',
+    videos.filter(
+      (item) => item.tag === profile && item.destination === destination,
+    )[0],
+  );
+
+  const backgroundVideo = videos.filter(
+    (item) => item.tag === profile && item.destination === destination,
+  )[0]?.url;
 
   return (
-  <div>
-    <Header />
-    <button onClick={handleBackClick}
+    <div>
+      <Header />
+      <button
+        onClick={handleBackClick}
+        style={{
+          position: 'absolute',
+          top: '80px',
+          right: '130px',
+          zIndex: '200',
+          padding: '10px 20px',
+          borderRadius: '5px',
+          border: 'none',
+          backgroundColor: '#333',
+          color: 'white',
+          cursor: 'pointer',
+        }}
+      >
+        Back
+      </button>
+
+      <div
+        style={{ position: 'relative', textAlign: 'center', color: 'white' }}
+      >
+        {
+          <video
+            src={backgroundVideo || ''}
+            autoPlay
+            loop
+            muted
+            style={{
+              width: '100%',
+              height: 'auto',
+              objectFit: 'cover',
+              zIndex: '-1',
+              backgroundColor: 'white',
+            }}
+          />
+        }
+
+        <div
           style={{
             position: 'absolute',
-            top:'80px',
-            right:'130px',
-            zIndex:'200',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: '1',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
             padding: '10px 20px',
             borderRadius: '5px',
-            border: 'none',
-            backgroundColor: '#333',
-            color: 'white',
-            cursor: 'pointer'    
           }}
         >
-      Back
-    </button>
-                  
-    <div style={{ position: 'relative', textAlign: 'center', color: 'white' }}>
-                {
-                <video
-                     src={
-                        backgroundVideo  || ""
-                     } 
-                     autoPlay
-                     loop
-                     muted
-                     style={{
-                       width: '100%',
-                       height: 'auto',
-                       objectFit: 'cover',
-                       zIndex: '-1',
-                       backgroundColor: "white"
-                     }}
-                />
-                }
-                
-              <div style={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                zIndex: '1',
-                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          <h1>Cheap flights to {destination} 42€</h1>
+          <div style={{ marginTop: '20px' }}>
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              style={{
+                padding: '10px',
+                borderRadius: '5px',
+                border: 'none',
+                marginRight: '10px',
+                width: '250px',
+              }}
+            />
+            <button
+              style={{
                 padding: '10px 20px',
-                borderRadius: '5px'
-              }}>
-                <h1>Cheap flights to {destination} 42€</h1>
-                <div style={{ marginTop: '20px' }}>
-                  <input
-                    type="text"
-                    placeholder="Where are you going?"
-                    style={{
-                      padding: '10px',
-                      borderRadius: '5px',
-                      border: 'none',
-                      marginRight: '10px',
-                      width: '250px'
-                    }}
-                  />
-                  <button
-                    style={{
-                      padding: '10px 20px',
-                      borderRadius: '5px',
-                      border: 'none',
-                      backgroundColor: 'red',
-                      color: 'white',
-                      cursor: 'pointer'
-                    }}
-                  >
-                    Search
-                  </button>
-                  
-                </div>
-              </div>
-            </div>
-  </div>
+                borderRadius: '5px',
+                border: 'none',
+                backgroundColor: 'red',
+                color: 'white',
+                cursor: 'pointer',
+              }}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default AdminPage;
-
