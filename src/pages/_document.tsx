01206cd1e9fbbac0523366393b75f510050f6ef9
javierqcWZ <javierqc@ext.inditex.com>
2024-07-16 11:41:32 +0200

    Meta header https http

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+Meta      <Head >
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+      </Head>  
       <body>
         <Main />
         <NextScript />
